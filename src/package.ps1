635155dc9c19e1761cdf26e135fd3353fd23ec39
Jerzy Adamowski <jerzyadamowski@outlook.com>
2020-03-15 20:39:16 +0100

    Renamed modes to full name

--- src/package.ps1
+++ src/package.ps1
@@ -1,38 +1,44 @@
 #todo: create scs file from this project
 param (
-  [Parameter(Mandatory=$true)][string]$outDir
+  [Parameter(Mandatory = $true)][string]$outDir
 )
 
-function 7zip([String] $aDirectory, [String] $aZipfile){
+function 7zip([String] $aDirectory, [String] $aZipfile) {
   [string]$pathToZipExe = "$($Env:ProgramFiles)\7-Zip\7z.exe";
   & "$pathToZipExe" a -tzip "$aZipfile" "$aDirectory" -r -mx=0
 }
 
-$inputDir = "$((Resolve-Path .\).Path)"
-$dirOut = Join-Path -Path $outDir -ChildPath NewEngineLevel
-$fileOutZip = Join-Path -Path $outDir -ChildPath NewEngineLevel.zip
-$fileOutScs = Join-Path -Path $outDir -ChildPath NewEngineLevel.scs
+$outDirectories = Get-ChildItem -Path .\out -Directory
 
-If(!(test-path $dirOut)) {
-  New-Item -ItemType Directory -Force -Path $dirOut | Out-Null
-} else {
-  Remove-Item -Path $dirOut -Force -Recurse
-  New-Item -ItemType Directory -Force -Path $dirOut | Out-Null
+foreach ($out in $outDirectories) {
+  $inputDir = $out.FullName
+  $dirOut = Join-Path -Path $outDir -ChildPath $out.Name
+  $fileOutZip = Join-Path -Path $outDir -ChildPath "$($out.Name).zip"
+  $fileOutScs = Join-Path -Path $outDir -ChildPath "$($out.Name).scs"
+
+  If (!(test-path $dirOut)) {
+    New-Item -ItemType Directory -Force -Path $dirOut | Out-Null
+  }
+  else {
+    Remove-Item -Path $dirOut -Force -Recurse
+    New-Item -ItemType Directory -Force -Path $dirOut | Out-Null
+  }
+
+  If ((test-path $fileOutZip)) {
+    Remove-Item -Path $fileOutZip -Force
+  }
+
+  If ((test-path $fileOutScs)) {
+    Remove-Item -Path $fileOutScs -Force
+  }
+
+  Copy-Item -Path "$inputDir\def" -Destination $dirOut -Recurse
+  Copy-Item -Path "$((Resolve-Path .\).Path)\mode\$($out.Name)\manifest.sii" -Destination $dirOut
+  Copy-Item -Path "$((Resolve-Path .\).Path)\mode\$($out.Name)\mod_description.txt" -Destination $dirOut
+  Copy-Item -Path "$((Resolve-Path .\).Path)\mode\$($out.Name)\newenginelevel.jpg" -Destination $dirOut
+
+  7zip -aDirectory "$dirOut\*" -aZipfile $fileOutZip
+
+  Rename-Item -Path $fileOutZip -NewName $fileOutScs
 }
 
-If((test-path $fileOutZip)) {
-  Remove-Item -Path $fileOutZip -Force
-}
-
-If((test-path $fileOutScs)) {
-  Remove-Item -Path $fileOutScs -Force
-}
-
-Copy-Item -Path "$inputDir\def" -Destination $dirOut -Recurse
-Copy-Item -Path "$inputDir\manifest.sii" -Destination $dirOut
-Copy-Item -Path "$inputDir\mod_description.txt" -Destination $dirOut
-Copy-Item -Path "$inputDir\newenginelevel.jpg" -Destination $dirOut
-
-7zip -aDirectory "$dirOut\*" -aZipfile $fileOutZip
-
-Rename-Item -Path $fileOutZip -NewName $fileOutScs
--- src/update.ps1
+++ src/update.ps1
@@ -1,40 +1,41 @@
 import-module psexcel
-$importData = Import-XLSX -Path .\src\NewEngineFormulas.xlsx -RowStart 18;
 
-$currentTruckPathSource = "";
-$currentTruckPathDest = "";
+$modeDirectories = Get-ChildItem -Path .\mode -Directory
 
-foreach($row in $importData)
-{
-  # get truck path
-  if ( [System.String]::IsNullOrWhiteSpace($row."Engine HP"))
-  {
-    $currentTruckPathSource = "$((Resolve-Path .\).Path)\src\def\vehicle\truck\$($row."Truck Name")\engine\"
-    $currentTruckPathDest = "$((Resolve-Path .\).Path)\def\vehicle\truck\$($row."Truck Name")\engine\"
-    continue;
+foreach ($dir in $modeDirectories) {
+  $importData = Import-XLSX -Path "$($dir.FullName)\NewEngineFormulas.xlsx" -RowStart 18;
+
+  $currentTruckPathSource = "";
+  $currentTruckPathDest = "";
+
+  foreach ($row in $importData) {
+    # get truck path
+    if ( [System.String]::IsNullOrWhiteSpace($row."Engine HP")) {
+      $currentTruckPathSource = "$((Resolve-Path .\).Path)\src\def\vehicle\truck\$($row."Truck Name")\engine\"
+      $currentTruckPathDest = "$((Resolve-Path .\).Path)\out\$($dir.Name)\def\vehicle\truck\$($row."Truck Name")\engine\"
+      continue;
+    }
+
+    #get engine path
+    if ( [System.Int32]::Parse($row."Engine HP") -gt 0) {
+      $currentEnginePathSource = "$currentTruckPathSource$($row."Truck Name")"
+      $currentEnginePathDest = "$currentTruckPathDest$($row."Truck Name")"
+    }
+
+    #if no more record end here
+    if ( [System.String]::IsNullOrWhiteSpace($row."Truck Name")) {
+      break;
+    }
+
+    #read content from file with replace placeholders within new level
+    $newContent = (Get-Content -Path $currentEnginePathSource) -replace "##AUTO##", "$($row."New Lvl")"
+
+    #write content to file
+    If (!(test-path $currentTruckPathDest)) {
+      New-Item -ItemType Directory -Force -Path $currentTruckPathDest | Out-Null
+    }
+
+    [System.IO.File]::WriteAllLines($currentEnginePathDest, $newContent) | Out-Null
   }
-
-  #get engine path
-  if ( [System.Int32]::Parse($row."Engine HP") -gt 0)
-  {
-    $currentEnginePathSource = "$currentTruckPathSource$($row."Truck Name")"
-    $currentEnginePathDest = "$currentTruckPathDest$($row."Truck Name")"
-  }
-
-  #if no more record end here
-  if ( [System.String]::IsNullOrWhiteSpace($row."Truck Name"))
-  {
-    break;
-  }
-
-  #read content from file with replace placeholders within new level
-  $newContent = (Get-Content -Path $currentEnginePathSource) -replace "##AUTO##", "$($row."New Lvl")"
-
-  #write content to file
-  If(!(test-path $currentTruckPathDest))
-  {
-    New-Item -ItemType Directory -Force -Path $currentTruckPathDest | Out-Null
-  }
-
-  [System.IO.File]::WriteAllLines($currentEnginePathDest, $newContent)| Out-Null
 }
+

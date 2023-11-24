406744098dab8143db70acc143d21f655e6dc92c
Jerzy Adamowski <jerzy.adamowski@ringieraxelspringer.pl>
2023-11-24 13:16:46 +0100

    part of scs is ready

--- src/package.ts
+++ src/package.ts
@@ -55,12 +55,9 @@ for (const mode of modes) {
 
   //create scs
   copy(path.join(mode, "default", "def"), path.join(modeInstallDir, "def"));
-  copy(path.join(mode, "default", "def", "manifest.sii"), modeInstallDir);
-  copy(
-    path.join(mode, "default", "def", "mod_description.txt"),
-    modeInstallDir
-  );
-  copy(path.join(mode, "default", "def", "image.jpg"), modeInstallDir);
+  copy(path.join(mode, "default", "manifest.sii"), modeInstallDir);
+  copy(path.join(mode, "default", "mod_description.txt"), modeInstallDir);
+  copy(path.join(mode, "default", "image.jpg"), modeInstallDir);
 
   if (zipOrNot()) {
     await compressDirectory(modeInstallDir, modeInstallZip);
